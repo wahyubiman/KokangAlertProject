080b587c05acc05a2bca9d5d96fd6409a8618c52
Wahyu Biman <41642070+wahyubiman@users.noreply.github.com>
2023-03-28 21:42:56 +0700

    fix error fetching binanceusdm api

--- backend/app/scheduler.py
+++ backend/app/scheduler.py
@@ -16,7 +16,7 @@ def scanner_msb_1h():
     run MSB scanner on 1h timeframe
     """
     webhook_url = os.getenv("DISCORD_WEBHOOK_MSB_1H")
-    dfs = ExchangeConnector("binanceusdm").data()
+    dfs = ExchangeConnector("binance", "future").data()
     result = MSB().result(dfs)
     if len(result["high"]) != 0 or len(result["low"]) != 0:
         send_discord_msb_msg(webhook_url, "MSB alert | 1h | Binance Future", result)
@@ -27,7 +27,7 @@ def scanner_msb_4h():
     run MSB scanner on 4h timeframe
     """
     webhook_url = os.getenv("DISCORD_WEBHOOK_MSB_4H")
-    dfs = ExchangeConnector("binanceusdm").data("4h")
+    dfs = ExchangeConnector("binance", "future").data("4h")
     result = MSB().result(dfs)
     if len(result["high"]) != 0 or len(result["low"]) != 0:
         send_discord_msb_msg(webhook_url, "MSB alert | 4h | Binance Future", result)
