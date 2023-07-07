7bc37769e7d9f730a8dd44e8cc6e677e0af57cb4
wahyu biman <wahyubiman@gmail.com>
2023-07-07 12:38:19 +0700

    add discord notification macd cross

--- backend/app/notification/discord.py
+++ backend/app/notification/discord.py
@@ -21,7 +21,7 @@ def discord_webhook_init(url: str) -> DiscordWebhook:
 
 def send_discord_msb_msg(url: str, title: str, messages: dict):
     """
-    send notification via discord webhook
+    send notification via discord webhook when msb break happen
 
     Args:
         url (str): webhook url
@@ -45,6 +45,41 @@ def send_discord_msb_msg(url: str, title: str, messages: dict):
             for msg_low in messages["low"]:
                 msb_low_text += "- " + msg_low + "\n"
             embed.add_embed_field(name="Low Break", value=msb_low_text)
+
+        embed.set_footer(text="Created by: Kokang")
+        embed.set_timestamp()
+        webhook.add_embed(embed)
+        webhook.execute()
+    except Exception as e:
+        capture_exception(e)
+
+
+def send_discord_macd_cross_msg(url: str, title: str, messages: dict):
+    """
+    send notification via discord webhook when macd cross happen
+
+    Args:
+        url (str): webhook url
+        title (str): title for message
+        messages (dict): dict with high & low key
+    """
+    try:
+        webhook = discord_webhook_init(url)
+        embed = DiscordEmbed(
+            title=title,
+            description="@everyone\n\n",
+            color="03b2f8",
+        )
+        macd_cross_high_text = ""
+        macd_cross_low_text = ""
+        if len(messages["high"]) != 0:
+            for msg_high in messages["high"]:
+                macd_cross_high_text += "- " + msg_high + "\n"
+            embed.add_embed_field(name="MACD Cross Down", value=macd_cross_high_text)
+        if len(messages["low"]) != 0:
+            for msg_low in messages["low"]:
+                macd_cross_low_text += "- " + msg_low + "\n"
+            embed.add_embed_field(name="MACD Cross High", value=macd_cross_low_text)
 
         embed.set_footer(text="Created by: Kokang")
         embed.set_timestamp()
