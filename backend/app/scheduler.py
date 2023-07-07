00a53c30b37b4db3c51fc5e41890a63cb62ad878
wahyu biman <wahyubiman@gmail.com>
2023-07-07 13:03:34 +0700

    add macd cross scheduler

--- backend/app/scheduler.py
+++ backend/app/scheduler.py
@@ -7,6 +7,7 @@ import os
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from app.indicator_alert.connector import ExchangeConnector
 from app.indicator_alert.strategy.msb import MSB
+from app.indicator_alert.strategy.macd import MacdCross
 from app.notification.discord import send_discord_msb_msg
 
 scheduler = AsyncIOScheduler()
@@ -40,13 +41,32 @@ def scanner_msb_4h():
         capture_exception(e)
 
 
+def scanner_macd_cross_5m():
+    """
+    run MSB scanner on 5m timeframe
+    """
+    try:
+        webhook_url = os.getenv("DISCORD_WEBHOOK_MACD_CROSS_5m")
+        dfs = ExchangeConnector("binance", "future").data()
+        result = MSB().result(dfs)
+        if len(result["high"]) != 0 or len(result["low"]) != 0:
+            send_discord_msb_msg(
+                webhook_url, "MACD Cross alert | 5m | Binance Future", result
+            )
+    except Exception as e:
+        capture_exception(e)
+
+
 def background_task():
     """
     start a background task
     """
     try:
-        scheduler.add_job(scanner_msb_1h, "cron", hour="0-23")  # run every hour
-        scheduler.add_job(scanner_msb_4h, "cron", hour="*/4")  # run every 4 hour
+        # scheduler.add_job(scanner_msb_1h, "cron", hour="0-23")  # run every hour
+        # scheduler.add_job(scanner_msb_4h, "cron", hour="*/4")  # run every 4 hour
+        scheduler.add_job(
+            scanner_macd_cross_5m, "cron", minute="*/5", hour="0-14"
+        )  # run macd every 5 minute in 0 - 14 UTC hour
         scheduler.start()  # start the scheduler
         # asyncio.get_event_loop().run_forever()
     except Exception as e:
